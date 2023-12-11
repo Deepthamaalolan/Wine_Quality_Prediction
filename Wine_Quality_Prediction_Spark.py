3acd370c278808b0e16e8aa101696f97d668a323
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2023-12-11 09:19:19 -0500

    Update Wine_Quality_Prediction_Spark.py

--- Wine_Quality_Prediction_Spark.py
+++ Wine_Quality_Prediction_Spark.py
@@ -7,36 +7,40 @@ from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml import PipelineModel
 from pyspark.ml.feature import StringIndexer
+from pyspark.mllib.tree import RandomForestModel
 from pyspark.sql.functions import col
 
 def clean_data(df): 
     return df.select(*(col(c).cast("double").alias(c.strip("\"")) for c in df.columns))
-
+"""main function for application"""
 if __name__ == "__main__":
     print("Starting Spark Application")
 
     spark = SparkSession.builder.appName("WineQualityPrediction").getOrCreate()
     
+    # create spark context to report logging information related spark
     sc = spark.sparkContext
     sc.setLogLevel('ERROR')
 
     print("Configuring Hadoop for S3")
     # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.amazonaws.com")
-    spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    # spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    spark.sparkContext._jsc.hadoopConfiguration().set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-    spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    # spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
     input_path = "s3://wimequalitypredictiondataset/ValidationDataset.csv"
     valid_path = "s3://wimequalitypredictiondataset/ValidationDataset.csv"
     model_path="s3://wimequalitypredictiondataset/trainedmodel"
- 
-    print(f"Reading CSV file from {input_path}")
+
+# read csv file in DataFram 
+    # print(f"Reading CSV file from {input_path}")
     df = (spark.read
           .format("csv")
           .option('header', 'true')
           .option("sep", ";")
           .option("inferschema",'true')
-          .load(input_path))
+          .load("./ValidationDataset.csv"))
     
     df1 = clean_data(df)
     all_features = ['fixed acidity',
@@ -50,7 +54,7 @@ if __name__ == "__main__":
                     ]
     
 
-rf = PipelineModel.load(model_path)
+rf = PipelineModel.load("./rf/")
     
 predictions = rf.transform(df1)
 results = predictions.select(['prediction', 'label'])
@@ -62,7 +66,7 @@ evaluator = MulticlassClassificationEvaluator(
                                             labelCol='label', 
                                             predictionCol='prediction', 
                                             metricName='accuracy')
-
+# printing accuracy of above model
 accuracy = evaluator.evaluate(predictions)
 print('Test Accuracy of wine prediction model = ', accuracy)
 metrics = MulticlassMetrics(results.rdd.map(tuple))
